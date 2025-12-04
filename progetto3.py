2f98bb18093af2de2d5579315888e15b84614db7
matteoliotti <matteo260403@gmail.com>
2025-12-04 14:02:01 +0100

    parte 1
    Variabili e Tipi di Dati

---
+++ progetto3.py
@@ -0,0 +1,8 @@
+#parte 1
+
+nome="Mario Rossi"
+eta=34
+saldo_conto=2500.75
+vip=True
+citta=["Roma","Milano","Napoli","Torino","Venezia"]
+viaggio={citta[0]:40,citta[1]:65,citta[2]:25,citta[3]:30,citta[4]:50}
